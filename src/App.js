94f36d51bb8919f036c1bb381f2ac005c0e52b12
[jkianese] <[jkianese@gmail.com]>
2018-10-02 13:39:33 -0400

    Map Not Rendering

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+// import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { GoogleApiWrapper } from 'google-maps-react' 
 import './App.css';
-import NeighborhoodMap from './components/NeighborhoodMap'
+import NeighborhoodMap from './components/NeighborhoodMap';
 
-class App extends Component {
+export class App extends Component {
   render () {
+    /*
+    if (!this.props.loaded) {
+      return <div>Loading...</div>
+    }
+    */
+     
     return (
       <main>
         <div id="map">
-          <NeighborhoodMap />
-        </div>
-      </main>  
+          <NeighborhoodMap google={this.props.google} /> 
+          {/* <NeighborhoodMap /> */}
+      </div>
+      </main>
     )
   }
 }
 
-export default App
+/*
+const MapContainer = React.createClass({
+  render: function() {
+    return <div>Google</div>;
+  }
+})
+*/
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBj5AzHYC1kUPRnvaT6G6zsAONHSpKmoqQ',
+  Async: true,
+  Defer: true 
+})(App)
--- src/components/NeighborhoodMap.js
+++ src/components/NeighborhoodMap.js
@@ -1,101 +1,41 @@
 import React, { Component } from 'react'
-// import ReactDOM from 'react-dom'
-import axios from 'axios'
+import ReactDOM from 'react-dom'
 
-class NeighborhoodMap extends Component {
-
-  state = {
-    venues: []
-  }
-
+export class NeighborhoodMap extends Component {
+  
   componentDidMount() {
-    this.getVenues()
-    // this.loadMap()
+      this.loadMap()
   }
 
-  loadMap = () => { // script on index.html. Can I ref that?
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBj5AzHYC1kUPRnvaT6G6zsAONHSpKmoqQ&callback=initMap")
-    window.initMap = this.initMap
-  }
+  loadMap() {
+    if (this.props && this.props.google) {
+      // google is available
+      const {google} = this.props;
+      const maps = google.maps;
 
-  getVenues = () => {
-    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
-    const parameters = {
-      client_id: "F1BIU3KU3RZFBQKCLKJ2MX1AT2ZRZFYRUXTJUMFGA1YUS5ZF",
-      client_secret: "N1HLP0MSWYJJATAS3CBQTTSZ2WLME5RB2TAUWHGE2UXZ5A1E",
-      query: "attractions", // WT used "food"
-      near: "Disney World",
-      v: "20180323"
+      const mapRef = this.refs.map;
+      const node = ReactDOM.findDOMNode(mapRef);
+      
+      let zoom = 8;
+      let lat = 28.385233;
+      let lng = -81.563873;
+
+      const center = new maps.LatLng(lat, lng);
+      const mapConfig = Object.assign({}, {
+        center: center,
+        zoom: zoom
+      })
+      this.map = new maps.Map(node, mapConfig);
     }
-
-    // From Walthrough video series
-    // Run: npm install axios
-    axios.get(endPoint + new URLSearchParams(parameters)) //URLSearchParams is actual function, don't change
-      .then(response => {
-        this.setState({
-          venues: response.data.response.groups[0].items
-        }, this.loadMap())
-      })
-      .catch(error => {
-        console.log("Error: " + error)
-      })
-
-  }
-
-  initMap = () => {
-
-    // create a map 
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 28.385233, lng: -81.563873},
-      zoom: 13
-    })
-    
-    // create an infowindow
-    var infowindow = new window.google.maps.InfoWindow()
-
-    // display dynamic markers
-    this.state.venues.map(myVenue => {
-
-    var contentString = `${myVenue.venue.name}` // Removed Place Name in WT 
-
-    var marker = new window.google.maps.Marker({
-      position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
-      map: map,
-      title: myVenue.venue.name
-    })
-
-    // click on a marker
-    marker.addListener('click', function() {
-      
-      // Change the Content
-      infowindow.setContent(contentString)
-      
-      // Open an InfoWindow
-      infowindow.open(map, marker)
-    })
-    
-  });
+  }  
   
-}      
-  
-  render() { 
+  render() {
     return (
-      <main>
-        <div id="map"></div>
-      </main>
+      <div ref='map'>
+        Loading map...
+      </div>
     )
   }
+  
 }
-
-// From Elharony WT video #2
-
-function loadScript(source) {
-  var index = window.document.getElementsByTagName("script")[0]
-  var script = window.document.createElement("script")
-  script.src = source
-  script.async = true
-  script.defer = true
-  index.parentNode.insertBefore(script, index)
-}
-    
 export default NeighborhoodMap
