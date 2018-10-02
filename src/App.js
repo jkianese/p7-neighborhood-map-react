a188733b354ca147fa6f8cb2e164c86f19f41ba8
[jkianese] <[jkianese@gmail.com]>
2018-10-01 21:51:30 -0400

    Basic Functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import axios from 'axios';
+// import map from './Map'
+import NeighborhoodMap from './components/NeighborhoodMap'
+
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <main>
+        <div id="map">
+          <NeighborhoodMap />
+        </div>
+      </main>  
+    )
   }
 }
 
-export default App;
+export default App
